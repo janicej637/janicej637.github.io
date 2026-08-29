0ad9109d699ec58586a35aed333b9084e090e4f6
janicej637 <122122328+janicej637@users.noreply.github.com>
2026-08-29 15:47:15 -0400

    Update main.js
    Signed-off-by: janicej637 <122122328+janicej637@users.noreply.github.com>

--- main.js
+++ main.js
@@ -229,7 +229,33 @@ function register(){
 	optC = document.getElementById("opt_c");
 	optD = document.getElementById("opt_d");
 
- 
+ // Answer labels
+var answers = document.querySelectorAll(".answer");
+
+answers.forEach(function(label) {
+
+    label.addEventListener("click", function() {
+
+        // Remove selected color from all labels
+        answers.forEach(function(item) {
+            item.classList.remove("selected");
+        });
+
+        // Make clicked label blue
+        this.classList.add("selected");
+
+        // Find the radio button connected to this label
+        var radioButton = document.getElementById(
+            this.getAttribute("for")
+        );
+
+        // Check the radio button
+        if (radioButton) {
+            radioButton.checked = true;
+        }
+    });
+
+});
 	
 } // end function 
 
