d8c74a06782acecf8f980ae0c37e400300b06c4a
janicej637 <122122328+janicej637@users.noreply.github.com>
2026-08-28 21:47:07 -0400

    Update main.js
    Signed-off-by: janicej637 <122122328+janicej637@users.noreply.github.com>

--- main.js
+++ main.js
@@ -4,7 +4,7 @@
 // Declare Application Variables 
 var checkFlag; 									// Reports if no option is selected
 var attemptTotal = -1; 							// Keeps track of total attempts
-var correctTotal = -1; 							// Keeps track of correct answers
+var correctTotal = 0; 							// Keeps track of correct answers
 var clickCounter = 0; 							// Keeps track of Submit clicks
 var correctAnswer = 0; 							// Position of correct answer
 var optA, optB, optC, optD; 					// Option variables for each radio button
